4be4a6dad7527fc9427275c5685a2724fc9deb48
Ramiro Romera <ramiromera25@gmail.com>
2025-11-02 19:17:36 -0300

    Clean not used code

--- src/main/java/ar/edu/utn/frc/tup/p4/repositories/DummyRepository.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frc.tup.p4.repositories;
-
-import org.springframework.stereotype.Repository;
-import rami.generic.entities.DummyEntity;
-
-@Repository
-public interface DummyRepository extends GenericRepository<DummyEntity, Long> {
-}
--- src/main/java/ar/edu/utn/frc/tup/p4/repositories/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frc.tup.p4.repositories;
-
-import org.springframework.stereotype.Repository;
-import rami.generic.entities.PersonEntity;
-
-@Repository
-public interface PersonRepository extends GenericRepository<PersonEntity, Long> {
-}

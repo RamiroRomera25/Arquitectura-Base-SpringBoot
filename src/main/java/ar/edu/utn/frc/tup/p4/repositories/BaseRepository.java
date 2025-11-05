4fcb116e9b5699200d03c7635cc91cebe98924b7
Romera Ramiro <ramiromera25@gmail.com>
2025-11-05 20:38:11 -0300

    Update BaseRepository.java

--- src/main/java/ar/edu/utn/frc/tup/p4/repositories/BaseRepository.java
+++ src/main/java/ar/edu/utn/frc/tup/p4/repositories/BaseRepository.java
@@ -2,8 +2,9 @@ package ar.edu.utn.frc.tup.p4.repositories;
 
 import ar.edu.utn.frc.tup.p4.entities.interfaces.BaseIdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface BaseRepository<T extends BaseIdEntity<ID>, ID extends Number> extends JpaRepository<T, ID> {
+public interface BaseRepository<T extends BaseIdEntity<ID>, ID extends Number> extends JpaRepository<T, ID>, JpaSpecificationExecutor<E> {
 }
